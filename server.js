6b64132cce11e0df22c1d3e96f2d327221695b89
rutvampatel <2202030400246@silveroakuni.ac.in>
2025-10-31 08:23:46 +0530

    Open CORS to allow all requests

--- server.js
+++ server.js
@@ -17,34 +17,8 @@ connectCloudinary()
 app.use(express.json())
 
 // --- CORS Configuration Block ---
-
-// 1. Define your allowed origins (CLEANED - NO HIDDEN SPACES)
-const allowedOrigins = [
-  "https://doctor-frontend-five.vercel.app",
-  "https://doctor-admin1.vercel.app",
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://localhost:5174"
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like Postman or mobile apps)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      // If the origin is in our whitelist, allow it
-      callback(null, true);
-    } else {
-      // Otherwise, block it
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-// 2. Use the new CORS options
-app.use(cors(corsOptions));
-
+// This line allows ALL requests from ANY origin
+app.use(cors())
 // --- End of CORS Block ---
 
 
